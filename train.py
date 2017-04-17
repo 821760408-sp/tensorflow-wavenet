4295e77a4b1b1aa901aa0de18478e433b09197d5
821760408-sp <821760408.sp@gmail.com>
2017-04-17 01:15:28 -0400

    reduced SAMPLE_SIZE; added local conditioning

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ NUM_STEPS = int(1e5)
 LEARNING_RATE = 1e-3
 WAVENET_PARAMS = './wavenet_params.json'
 STARTED_DATESTRING = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.now())
-SAMPLE_SIZE = 100000
+SAMPLE_SIZE = 10000
 L2_REGULARIZATION_STRENGTH = 0
 SILENCE_THRESHOLD = 0.02  # 0.02 for our piano corpus
 EPSILON = 0.001
@@ -100,6 +100,8 @@ def get_arguments():
                         help='Whether to store histogram summaries. Default: False')
     parser.add_argument('--gc_channels', type=int, default=None,
                         help='Number of global condition channels. Default: None. Expecting: Int')
+    parser.add_argument('--lc_channels', type=int, default=1025,
+                        help='Number of local condition channels. Default: 1024. Expecting: Int')
     parser.add_argument('--max_checkpoints', type=int, default=MAX_TO_KEEP,
                         help='Maximum amount of checkpoints that will be kept alive. Default: '
                              + str(MAX_TO_KEEP) + '.')
@@ -208,18 +210,20 @@ def main():
     # Create coordinator.
     coord = tf.train.Coordinator()
 
-    # Load raw waveform from VCTK corpus.
+    # Load raw waveform from corpus.
     with tf.name_scope('create_inputs'):
         # Allow silence trimming to be skipped by specifying a threshold near
         # zero.
         silence_threshold = args.silence_threshold if args.silence_threshold > \
                                                       EPSILON else None
         gc_enabled = args.gc_channels is not None
+        lc_enabled = args.lc_channels is not None
         reader = AudioReader(
             args.data_dir,
             coord,
             sample_rate=wavenet_params['sample_rate'],
             gc_enabled=gc_enabled,
+            lc_enabled=lc_enabled,
             receptive_field=WaveNetModel.calculate_receptive_field(wavenet_params["filter_width"],
                                                                    wavenet_params["dilations"],
                                                                    wavenet_params["scalar_input"],
@@ -231,6 +235,10 @@ def main():
             gc_id_batch = reader.dequeue_gc(args.batch_size)
         else:
             gc_id_batch = None
+        if lc_enabled:
+            lc_embedding_batch = reader.dequeue_lc(args.batch_size)
+        else:
+            lc_embedding_batch = None
 
     # Create network.
     net = WaveNetModel(
@@ -246,12 +254,14 @@ def main():
         initial_filter_width=wavenet_params["initial_filter_width"],
         histograms=args.histograms,
         global_condition_channels=args.gc_channels,
-        global_condition_cardinality=reader.gc_category_cardinality)
+        global_condition_cardinality=reader.gc_category_cardinality,
+        local_condition_channels=args.lc_channels)
 
     if args.l2_regularization_strength == 0:
         args.l2_regularization_strength = None
     loss = net.loss(input_batch=audio_batch,
                     global_condition_batch=gc_id_batch,
+                    local_condition_batch=lc_embedding_batch,
                     l2_regularization_strength=args.l2_regularization_strength)
     optimizer = optimizer_factory[args.optimizer](
                     learning_rate=args.learning_rate,

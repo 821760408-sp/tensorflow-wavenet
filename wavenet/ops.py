cf713957148b5a4ad1ace208b98b4bdbd8ba6de1
821760408-sp <821760408.sp@gmail.com>
2017-04-22 07:03:49 -0400

    use Nsynth's WaveNet methods

--- wavenet/ops.py
+++ wavenet/ops.py
@@ -24,44 +24,69 @@ optimizer_factory = {'adam': create_adam_optimizer,
                      'sgd': create_sgd_optimizer,
                      'rmsprop': create_rmsprop_optimizer}
 
-
-def time_to_batch(value, dilation, name=None):
+# https://github.com/tensorflow/magenta/blob/master/magenta/models/nsynth/wavenet/masked.py#L53
+def time_to_batch(x, dilation):
     with tf.name_scope('time_to_batch'):
-        shape = tf.shape(value)
-        pad_elements = dilation - 1 - (shape[1] + dilation - 1) % dilation
-        padded = tf.pad(value, [[0, 0], [0, pad_elements], [0, 0]])
-        reshaped = tf.reshape(padded, [-1, dilation, shape[2]])
-        transposed = tf.transpose(reshaped, perm=[1, 0, 2])
-        return tf.reshape(transposed, [shape[0] * dilation, -1, shape[2]])
+        shape = x.get_shape().as_list()
+        y = tf.reshape(x, [
+            shape[0], shape[1] / dilation, dilation, shape[2]
+        ])
+        y = tf.transpose(y, [0, 2, 1, 3])
+        y = tf.reshape(y, [
+            shape[0] * dilation, shape[1] / dilation, shape[2]
+        ])
+        y.set_shape([
+            mul_or_none(shape[0], dilation), mul_or_none(shape[1], 1. / dilation),
+            shape[2]
+        ])
+        return y
 
-
-def batch_to_time(value, dilation, name=None):
+# https://github.com/tensorflow/magenta/blob/master/magenta/models/nsynth/wavenet/masked.py#L85
+def batch_to_time(x, dilation):
     with tf.name_scope('batch_to_time'):
-        shape = tf.shape(value)
-        prepared = tf.reshape(value, [dilation, -1, shape[2]])
-        transposed = tf.transpose(prepared, perm=[1, 0, 2])
-        return tf.reshape(transposed,
-                          [tf.div(shape[0], dilation), -1, shape[2]])
+        shape = x.get_shape().as_list()
+        y = tf.reshape(x, [shape[0] / dilation, dilation, shape[1], shape[2]])
+        y = tf.transpose(y, [0, 2, 1, 3])
+        y = tf.reshape(y, [shape[0] / dilation, shape[1] * dilation, shape[2]])
+        y.set_shape([mul_or_none(shape[0], 1. / dilation),
+                     mul_or_none(shape[1], dilation),
+                     shape[2]])
+        return y
 
+# https://github.com/tensorflow/magenta/blob/master/magenta/models/nsynth/wavenet/masked.py#L106
+def conv1d(x,
+           filt,
+           biases,
+           dilation=1,
+           causal=True):
+    """Fast 1D convolution that supports causal padding and dilation.
+    
+      :param x: the [bs, time, input_channes] float input tensor
+      :param filt: the filter we convolve x with
+      :param biases: the biases
+      :param dilation: the amount of dilation
+      :param causal: whether it's a causal convolution or not
+      :return: the 1D convolution output
+    """
+    batch_size, length, num_input_channels = x.get_shape().as_list()
+    filter_width, filt_in_channels, filt_out_channels = filt.get_shape().as_list()
+    assert length % dilation == 0
+    assert num_input_channels == filt_in_channels
 
-def causal_conv(value, filter_, dilation, name='causal_conv'):
-    with tf.name_scope(name):
-        filter_width = tf.shape(filter_)[0]
-        if dilation > 1:
-            transformed = time_to_batch(value, dilation)
-            conv = tf.nn.conv1d(transformed, filter_, stride=1,
-                                padding='VALID')
-            restored = batch_to_time(conv, dilation)
-        else:
-            restored = tf.nn.conv1d(value, filter_, stride=1, padding='VALID')
-        # Remove excess elements at the end.
-        out_width = tf.shape(value)[1] - (filter_width - 1) * dilation
-        result = tf.slice(restored,
-                          [0, 0, 0],
-                          [-1, out_width, -1])
-        return result
+    strides = [1, 1, 1]
+    padding = 'VALID' if causal else 'SAME'
 
+    x_ttb = time_to_batch(x, dilation)
+    if filter_width > 1 and causal:
+        x_ttb = tf.pad(x_ttb, [[0, 0], [filter_width - 1, 0], [0, 0]])
 
+    y = tf.nn.conv1d(x_ttb, filt, strides, padding)
+    y = tf.nn.bias_add(y, biases)
+    y = batch_to_time(y, dilation)
+    y.set_shape([batch_size, length, filt_out_channels])
+    return y
+
+# https://github.com/tensorflow/magenta/blob/master/magenta/models/nsynth/utils.py#L64
 def mu_law_encode(audio, quantization_channels=256):
     """Quantizes waveform amplitudes."""
     with tf.name_scope('encode'):
@@ -70,7 +95,7 @@ def mu_law_encode(audio, quantization_channels=256):
         out = tf.cast(tf.floor(out * 128), tf.int8)
         return out
 
-
+# https://github.com/tensorflow/magenta/blob/master/magenta/models/nsynth/utils.py#L79
 def mu_law_decode(x, quantization_channels=256):
     """Recovers waveform from quantized values."""
     with tf.name_scope('decode'):
@@ -80,3 +105,36 @@ def mu_law_decode(x, quantization_channels=256):
         out = tf.sign(out) / mu * ((1 + mu)**tf.abs(out) - 1)
         out = tf.where(tf.equal(x, 0), x, out)
         return out
+
+# https://github.com/tensorflow/magenta/blob/master/magenta/models/nsynth/wavenet/masked.py#L36
+def mul_or_none(a, b):
+  """Return the element wise multiplicative of the inputs.
+
+  If either input is None, we return None.
+
+  Args:
+    a: A tensor input.
+    b: Another tensor input with the same type as a.
+
+  Returns:
+    None if either input is None. Otherwise returns a * b.
+  """
+  if a is None or b is None:
+    return None
+  return a * b
+
+# https://github.com/tensorflow/magenta/blob/master/magenta/models/nsynth/wavenet/masked.py#L20
+def shift_right(x):
+  """Shift the input over by one and a zero to the front.
+
+  Args:
+    x: The [mb, time, channels] tensor input.
+
+  Returns:
+    x_sliced: The [mb, time, channels] tensor output.
+  """
+  shape = x.get_shape().as_list()
+  x_padded = tf.pad(x, [[0, 0], [1, 0], [0, 0]])
+  x_sliced = tf.slice(x_padded, [0, 0, 0], tf.stack([-1, shape[1], -1]))
+  x_sliced.set_shape(shape)
+  return x_sliced
